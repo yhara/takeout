cb9ae208c04467314611750c8bb4687dea33926c
Yutaka HARA <yutaka.hara.gmail.com>
2012-07-20 18:42:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Takeout::Application.config.secret_token = 'e3693befa80e48c45972ee8e7fa2c3bdda7a400936d2db6ba25ab895f34024509cf47fe77fbf97bf2cc630ef6cba99e722bc7592142b6ba274eaf96cf565d047'
