1537c3df9dc484ec93a6822acc20f2b1722bf954
Yutaka HARA <yutaka.hara.gmail.com>
2012-08-03 00:05:50 +0900

    change: Use "r123" as keys for svn repository

--- lib/repository.rb
+++ lib/repository.rb
@@ -18,25 +18,28 @@ class Repository
   class SvnRepos < Repository
     # Retuns an array of Commit (old one first)
     def fetch_commits
-      last_rev = Commit.order("key DESC").first.try(:key).try(:to_i) || 0
-      next_rev = last_rev + 1
-      command "svn up"
+      next_rev = if (last_commit = Commit.order("key DESC").first)
+                   last_commit.key[/\d+/].to_i + 1
+                 else
+                   1
+                 end
+      command("svn up")
       latest_rev = command("svn info")[/Revision: (\d+)/, 1].to_i
 
       revs = (next_rev..latest_rev).to_a.last(MAX_FETCH)
       return revs.map{|rev|
-        fetch_commit(rev)
+        fetch_commit("r#{rev}")
       }
     end
 
     def fetch_commit(key)
-      rev = key.to_i
+      rev = key[/\d+/].to_i
       diff = svn_diff(rev)
       log = svn_log(rev)
       message = log.lines.to_a[3..-3].join
       commited_at = DateTime.parse(log.lines.to_a[1].split(/\|/)[2])
 
-      return Commit.new(key: rev.to_s,
+      return Commit.new(key: "r#{rev}",
                         log: message,
                         diff: diff,
                         commited_at: commited_at)
