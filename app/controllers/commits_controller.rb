8c8b19c1834fa85f4a54bfeae74041af0ad016df
Yutaka HARA <yutaka.hara.gmail.com>
2012-08-03 00:31:16 +0900

    fix: latest comment shoulbe last

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -14,7 +14,7 @@ class CommitsController < ApplicationController
   # GET /commits/1.json
   def show
     @commit = Commit.find(params[:id])
-    @notes = @commit.notes.order("created_at DESC")
+    @notes = @commit.notes.order("created_at")
     @note = Note.new
 
     respond_to do |format|
