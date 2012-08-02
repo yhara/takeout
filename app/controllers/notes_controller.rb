58d8861f0b14f13b45787f99aa39b44023fe7aa1
Yutaka HARA <yutaka.hara.gmail.com>
2012-08-03 00:39:18 +0900

    new: update commit status by writing "#foo" at first line of note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -46,6 +46,8 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
+        @commit.update_status!(@note.body)
+
         format.html { redirect_to @commit, notice: 'Note was successfully created.' }
         format.json { render json: @commit, status: :created, location: @commit }
       else
--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,4 +3,11 @@ class Commit < ActiveRecord::Base
   attr_accessible :commited_at, :diff, :key, :log, :status
 
   validates :key, uniqueness: true
+
+  def update_status!(note_body)
+    if (newstat = note_body.lines.first[/^#(.*)/, 1])
+      self.status = newstat
+      self.save!
+    end
+  end
 end
