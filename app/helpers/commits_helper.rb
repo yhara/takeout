f5b37faba15f14548d4011a3d2a7329dcd91ad42
Yutaka HARA <yutaka.hara.gmail.com>
2012-08-03 12:35:27 +0900

    Set time zone in config

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -2,4 +2,8 @@ module CommitsHelper
   def format_note_body(txt)
     txt.lines.map{|l| "#{h l}<br>\n"}.join.html_safe
   end
+
+  def zone(datetime)
+    datetime.in_time_zone(Takeout::Conf[:time_zone])
+  end
 end
