e412fc539c90f54b77a45ab6ba916d317ee17796
Yutaka HARA <yutaka.hara.gmail.com>
2012-08-06 23:42:37 +0900

    Remove json support from controllers

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,25 +1,11 @@
 class CommitsController < ApplicationController
-  # GET /commits
-  # GET /commits.json
   def index
     @commits = Commit.order("created_at DESC")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @commits }
-    end
   end
 
-  # GET /commits/1
-  # GET /commits/1.json
   def show
     @commit = Commit.find(params[:id])
     @notes = @commit.notes.order("created_at")
     @note = Note.new
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @commit }
-    end
   end
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,4 @@
 class NotesController < ApplicationController
-  # POST /notes
-  # POST /notes.json
   def create
     @commit = Commit.find(params[:commit_id])
     @note = Note.new(params[:note])
@@ -10,11 +8,9 @@ class NotesController < ApplicationController
       if @note.save
         @commit.update_status!(@note.body)
 
-        format.html { redirect_to @commit, notice: 'Note was successfully created.' }
-        format.json { render json: @commit, status: :created, location: @commit }
+        redirect_to @commit, notice: 'Note was successfully created.'
       else
-        format.html { render action: "new" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
+        render action: "new"
       end
     end
   end
